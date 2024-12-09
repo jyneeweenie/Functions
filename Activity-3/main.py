28f99b5bb3e6111091ddced2d534698d8b6546f4
jyneeweenie <jynemarie1@gmail.com>
2024-12-09 10:14:32 +0300

    correctiions are done

--- Activity-3/main.py
+++ Activity-3/main.py
@@ -1,18 +1,19 @@
 # Program make a simple calculator
-# This function add two numbers def add(x,y):
+# This function add two numbers
+def add(x,y):
     return x + y
 # This function subtracts two numbers
 def subtract(x, y):
     return x- y
 # This function multiplies two numbers
-def multiplies(x, y):
+def multiply(x, y):
     return x * y
 # This function divides two numbers
-def divides(x, y):
+def divide(x, y):
     return x / y
 num1 = int(input("Enter Number 1: "))
 num2 = int(input("Enter Number 2: "))
 print("Sum :", add(num1, num2))
-print("Differences :"subtract(num1, num2))
-print("Product :"multiply(num1, num2))
-print("Quotient :"divide(num1, num2))
+print("Differences :" , subtract(num1, num2))
+print("Product :", multiply(num1, num2))
+print("Quotient :", divide(num1, num2))
